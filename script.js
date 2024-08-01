cf258a3f055f265e1dad8676ddcfe55dea336be2
William <willplacido40@gmail.com>
2024-07-31 21:36:43 -0300

    create script

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function criptText(){
+    let text_user = document.getElementById('user-text')
+
+    let char = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+    let char_atbash = ["z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a"]
+
+
+}
