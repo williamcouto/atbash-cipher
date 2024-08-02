bff281cd22513d5559a5f4359d2de885d4a70081
William <willplacido40@gmail.com>
2024-08-02 16:24:31 -0300

    update JS

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 function criptText(){
-    let text_user = document.getElementById('user-text')
-
+    let text_user = document.getElementById('user-text').value
     let char = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
-    let char_atbash = ["z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a"]
+    let char_cript = ["z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a"]
+    let result_text = document.querySelector('#text-decode')
 
+   char.forEach(function(char){
+        if(text_user[i]){
 
+        }
+   })
 }
