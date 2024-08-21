13b59a7495be6ab560a468e41cd08bbc750261b2
williamcouto <willplacido40@gmail.com>
2024-08-21 20:11:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let text_encode = document.getElementById('text_encode')
 
 function encodeAtbash(text){
     let text_apart = text.split('')
-    
+
+    for(let i = 0 ; i < text_apart.length ; i++){
+        let index = alphab.indexOf(text_apart[i])
+
+        if(index !== -1){
+            text_encrypted += alphaInverted[index]
+        }
+        else{
+            text_encrypted += text_apart[i]
+        }
+    }
+    return text_encrypted
 }
 
