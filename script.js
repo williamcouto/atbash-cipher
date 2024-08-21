97f8692adfb5012d5e3496e44ef55ad408479f1e
williamcouto <willplacido40@gmail.com>
2024-08-21 20:07:36 -0300

    add script

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+let user_text = document.getElementById('user-text')
+let alphab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let alphaInverted = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+let text_encrypted = ''
+let text_encode = document.getElementById('text_encode')
 
-function criptText(){
-    let text_user = document.getElementById('user-text').value
-    let char = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
-    let char_cript = ["z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a"]
-    let result_text = document.querySelector('#text-decode')
-
-   char.forEach(function(char){
-        if(text_user[i]){
-
-        }
-   })
+function encodeAtbash(text){
+    let text_apart = text.split('')
+    
 }
+
