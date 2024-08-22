b3063fdb92482707fb01f7c59c188830f6ec4acc
williamcouto <willplacido40@gmail.com>
2024-08-21 21:16:57 -0300

    Update HTML and JS
    fix textarea and code

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let user_text = document.getElementById('user-text')
-let alphab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let alphaInverted = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
-let text_encrypted = ''
-let text_encode = document.getElementById('text_encode')
+function encodeAtbash(){
+    let userText = document.getElementById('user_text').value
+    let alphab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let alphaInverted = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+    let text_crypto_res = document.querySelector('#text_encode')
+    let text_encrypted = ''
 
-function encodeAtbash(text){
-    let text_apart = text.split('')
+    function encode(text){
+        let text_apart = text.split('')
+        
+        for(let i = 0 ; i < text_apart.length ; i++){
+            let index = alphab.indexOf(text_apart[i])
 
-    for(let i = 0 ; i < text_apart.length ; i++){
-        let index = alphab.indexOf(text_apart[i])
-
-        if(index !== -1){
-            text_encrypted += alphaInverted[index]
+            if(index !== -1){
+                text_encrypted += alphaInverted[index]
+            }
+            else{
+                text_encrypted += text_apart[i]
+            }
         }
-        else{
-            text_encrypted += text_apart[i]
-        }
+        return text_encrypted
     }
-    return text_encrypted
+    text_encrypted = encode(userText)
+    text_crypto_res.textContent = text_encrypted
 }
-
